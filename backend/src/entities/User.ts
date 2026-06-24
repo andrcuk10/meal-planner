f210aa0eb283cf78a7a8a53f7d94d4fd852811dc
Andrija Cuk <andr.cuk10@gmail.com>
2026-06-24 13:36:31 +0200

    feat: add User entity

---
+++ backend/src/entities/User.ts
@@ -0,0 +1,19 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+}
