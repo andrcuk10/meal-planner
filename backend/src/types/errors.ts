7e22b5ace6e3cf6ee744e0bca239a23a01d1dfa4
Andrija Cuk <andr.cuk10@gmail.com>
2026-06-24 20:44:58 +0200

    feat: error types for API

---
+++ backend/src/types/errors.ts
@@ -0,0 +1,17 @@
+export type ErrorCode =
+  | 'INVALID_DATA'
+  | 'INCORRECT_CREDENTIALS'
+  | 'NO_SUCH_ENTITY'
+  | 'NOT_YOURS'
+  | 'NOT_AUTHENTICATED'
+  | 'DUPLICATE_EMAIL'
+  | 'DUPLICATE_USERNAME'
+  | 'INTERNAL_ERROR';
+
+export type APIErrorCommon = {
+  code: ErrorCode;
+};
+
+export type APIError = APIErrorCommon;
+
+// export type APIError = APIErrorCommon | (APIErrorCommon & { extra: ZodIssue[] });
