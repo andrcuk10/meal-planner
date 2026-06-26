9664227041ee60d02213d5db36d0e201daa7fcf9
Andrija Cuk <andr.cuk10@gmail.com>
2026-06-26 22:18:00 +0200

    feat: authenticate all Ingredient routes

--- backend/src/routes/ingredient.routes.ts
+++ backend/src/routes/ingredient.routes.ts
@@ -2,13 +2,24 @@ import { Router } from 'express';
 import { IngredientController } from '../controllers/ingredient.controller';
 import { validate } from '../middleware/validate';
 import { createIngredientSchema, updateIngredientSchema } from '../schemas/ingredient.schema';
+import { authenticate } from '../middleware/authenticate';
 
 const router = Router();
 
-router.get('/', IngredientController.getAllIngredients);
-router.get('/:id', IngredientController.getSingleIngredient);
-router.post('/', validate(createIngredientSchema), IngredientController.createIngredient);
-router.patch('/:id', validate(updateIngredientSchema), IngredientController.updateIngredient);
-router.delete('/:id', IngredientController.deleteIngredient);
+router.get('/', authenticate, IngredientController.getAllIngredients);
+router.get('/:id', authenticate, IngredientController.getSingleIngredient);
+router.post(
+  '/',
+  authenticate,
+  validate(createIngredientSchema),
+  IngredientController.createIngredient,
+);
+router.patch(
+  '/:id',
+  authenticate,
+  validate(updateIngredientSchema),
+  IngredientController.updateIngredient,
+);
+router.delete('/:id', authenticate, IngredientController.deleteIngredient);
 
 export default router;
