e0d27229cbf07b5c67ed897cc1e364035805a8cd
Andrija Cuk <andr.cuk10@gmail.com>
2026-06-28 10:55:02 +0200

    feat: add RecipeIngredient entity(junction table)

---
+++ backend/src/entities/RecipeIngredient.ts
@@ -0,0 +1,47 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Recipe } from './Recipe';
+import { Ingredient } from './Ingredient';
+
+export enum UnitMeasure {
+  TEASPOON = 'tsp.',
+  TABLESPOON = 'tbsp.',
+  CUP = 'cup',
+  MILLILITER = 'ml',
+  LITER = 'l',
+  GRAM = 'gr',
+  KILOGRAM = 'kg',
+}
+
+@Entity('recipes_ingredients')
+export class RecipeIngredient {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ insert: false, update: false })
+  recipe_id: string;
+
+  @Column({ insert: false, update: false })
+  ingredient_id: string;
+
+  @Column()
+  quantity: number;
+
+  @Column({
+    type: 'enum',
+    enum: UnitMeasure,
+  })
+  unit: UnitMeasure;
+
+  @Column({ nullable: true })
+  note?: string | null;
+
+  @ManyToOne(() => Recipe, (recipe) => recipe.recipeIngredients, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'recipe_id' })
+  recipe: Recipe;
+
+  @ManyToOne(() => Ingredient, (ingredient) => ingredient.recipeIngredients, {
+    onDelete: 'RESTRICT',
+  })
+  @JoinColumn({ name: 'ingredient_id' })
+  ingredient: Ingredient;
+}
