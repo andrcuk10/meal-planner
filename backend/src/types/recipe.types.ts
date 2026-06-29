858a1044a2e0693c7ea414228299a9c5a35f9f05
Andrija Cuk <andr.cuk10@gmail.com>
2026-06-29 12:59:56 +0200

    refactor: change RecipeIngredient types

--- backend/src/types/recipe.types.ts
+++ backend/src/types/recipe.types.ts
@@ -3,19 +3,19 @@ import { UnitMeasure } from '../entities/RecipeIngredient';
 export type BaseIngredientDto = { name: string };
 export type IngredientDto = BaseIngredientDto & { ingredient_id: string };
 
-export type RecipeIngredient = {
+export type BaseRecipeIngredient = {
   ingredient: { ingredient_id: string; name: string };
   quantity: number;
   unit: UnitMeasure;
   note?: string;
 };
-export type RecipeIngredientDto = RecipeIngredient & { recipe_ingredient_id: string };
+export type RecipeIngredientDto = BaseRecipeIngredient & { recipe_ingredient_id: string };
 
 export type BaseStep = { step_num: number; step_text: string };
 export type StepDto = BaseStep & { step_id: string; recipe_id: string };
 
 export type BaseRecipeDto = CreateRecipeDto & {
-  ingredients: RecipeIngredient[];
+  ingredients: BaseRecipeIngredient[];
   steps: BaseStep[];
 };
 export type RecipeDto = BaseRecipeDto & {
