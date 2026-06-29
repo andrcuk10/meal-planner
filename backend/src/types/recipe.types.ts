e32b15b09cbc36868dd24b432bb3a403d75285bf
Andrija Cuk <andr.cuk10@gmail.com>
2026-06-29 11:47:05 +0200

    feat: add Recipe types

---
+++ backend/src/types/recipe.types.ts
@@ -0,0 +1,30 @@
+import { UnitMeasure } from '../entities/RecipeIngredient';
+
+export type BaseIngredientDto = { name: string };
+export type IngredientDto = BaseIngredientDto & { ingredient_id: string };
+
+export type RecipeIngredient = {
+  ingredient: { ingredient_id: string; name: string };
+  quantity: number;
+  unit: UnitMeasure;
+  note?: string;
+};
+export type RecipeIngredientDto = RecipeIngredient & { recipe_ingredient_id: string };
+
+export type BaseStep = { step_num: number; step_text: string };
+export type StepDto = BaseStep & { step_id: string; recipe_id: string };
+
+export type BaseRecipeDto = CreateRecipeDto & {
+  ingredients: RecipeIngredient[];
+  steps: BaseStep[];
+};
+export type RecipeDto = BaseRecipeDto & {
+  recipe_id: string;
+  creator: { user_id: string; username: string };
+  createdAt: Date | string;
+};
+
+export type CreateRecipeDto = {
+  name: string;
+  isPublic: boolean;
+};
